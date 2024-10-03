9effb8824e04190710755d4b232f06a84effa6bb
Alan Robb <robbalan61@gmail.com>
2024-10-03 18:07:38 +0100

    Updated js file 03/10

--- ClientSideScripting/Practical/QUIZ ARRAYS/js/standardinputquiz.js
+++ ClientSideScripting/Practical/QUIZ ARRAYS/js/standardinputquiz.js
@@ -2,7 +2,7 @@ let questions = [
     'Q1: What is the capital city of Scotland?',
     'Q2: What is the capital city of Ireland?',
     'Q3: What is the capital city of Peru?',
-    'Q4: Explain the space time continuum.'
+    'Q4: What is the capital city of France?'
 ];
 
 /* let getAnswers[]
@@ -27,9 +27,41 @@ function checkAnswers(){
     if(ansQ0.toLowerCase() === 'edinburgh'){
         counter++;//increment counter for correct answer
         document.getElementById('q0').style.backgroundColor = 'green';
-
+        document.getElementById('q0').innerHTML = 'Correct, the capital city of Scotland is Edinburgh';
+    }else{
+        document.getElementById('q0').style.backgroundColor = 'red';
+        document.getElementById('q0').innerHTML = 'Incorrect, the capital city of Scotland is Edinburgh';
+    
     }
 
+    if(ansQ1.toLowerCase() === 'dublin'){
+        counter++;//increment counter for correct answer
+        document.getElementById('q1').style.backgroundColor = 'green';
+        document.getElementById('q1').innerHTML = 'Correct, the capital city of Ireland is Dublin';
+    }else{
+        document.getElementById('q1').style.backgroundColor = 'red';
+        document.getElementById('q1').innerHTML = 'Incorrect, the capital city of Ireland is Dublin';
     
-}
+    }
+    if(ansQ2.toLowerCase() === 'lima'){
+        counter++;//increment counter for correct answer
+        document.getElementById('q2').style.backgroundColor = 'green';
+        document.getElementById('q2').innerHTML = 'Correct, the capital city of Peru is Lima';
+    }else{
+        document.getElementById('q2').style.backgroundColor = 'red';
+        document.getElementById('q2').innerHTML = 'Incorrect, the capital city of Peru is Lima';
+    
+    }
+    if(ansQ3.toLowerCase() === 'paris'){
+        counter++;//increment counter for correct answer
+        document.getElementById('q3').style.backgroundColor = 'green';
+        document.getElementById('q3').innerHTML = 'Correct, the capital city of France is Paris';
+    }else{
+        document.getElementById('q3').style.backgroundColor = 'red';
+        document.getElementById('q3').innerHTML = 'Incorrect, the capital city of France is Paris';
+    
+    }
 
+    document.getElementById('score').innerHTML = 'Score:' + counter;
+
+}
