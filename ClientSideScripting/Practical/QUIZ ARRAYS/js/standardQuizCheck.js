3f96e29b9290cf2bf9b4808a7567725d96082d01
Alan Robb <robbalan61@gmail.com>
2024-10-21 23:29:02 +0100

    completed javascript for check box quiz

--- ClientSideScripting/Practical/QUIZ ARRAYS/js/standardQuizCheck.js
+++ ClientSideScripting/Practical/QUIZ ARRAYS/js/standardQuizCheck.js
@@ -20,7 +20,45 @@ function checkAnswers(){
     //Loop through each question
     for(var i=0; i<questions.length; i++){
         var checkboxes = document.querySelectorAll('input[name="q'+ i +'"]:checked');//Get checked boxes for current question
-        
+
+        //If at least one checkbox is checked for the current question
+        if(checkboxes.length > 0){
+            var correctAnswers = getCorrectAnswer(i);//Get correct answer for current question
+            var userAnswers = []; //Array to store user-selected answers
+
+            //Push value of each checked box to userAnswers array
+            checkboxes.forEach(function(checkbox){
+                userAnswers.push(checkbox.value);
+            });
+
+            //Check if userAnswers array contains all correct answers for the current question
+            if(correctAnswers.every(answer >= userAnswers.includes(answer))){
+                counter++;//Increment counter if all correct answers are selected
+                document.getElementById('q'+i).style.backgroundColor = "green";//Provide feedback
+                document.getElementById('q'+i).innerHTML = 'Correct! The capital city of Scotland is Edinburgh';//Feedback correct answer
+
+            } else{
+                document.getElementById('q'+i).style.backgroundColor = 'red';//Provide feedback
+                document.getElementById('q'+i).innerHTML = 'Incorrect! The capital city of Scotland is Edinburgh.';//Feedback correct answer
+            }
+        }
+    }
+
+    //Display the score
+    document.getElementById('score').innerHTML = "Score: "+ counter;
+}
+
+//Function to get correct answers for a given question index
+function getCorrectAnswers(questionIndex){
+    switch(questionIndex){
+        case 0:
+            return['edinburgh'];//Correct answer(s) for question 1
+        case 1:
+            return['dublin'];
+        case 2:
+            return['lima'];
+        default:
+            return[];
     }
 }
 
