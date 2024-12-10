ea86399b120fe78570321888a6716c2d1b086d8e
Alan Robb <robbalan61@gmail.com>
2024-12-10 00:47:17 +0000

    Updated images and menu.html

--- dignans_site/js/menu.js
+++ dignans_site/js/menu.js
@@ -1,9 +1,91 @@
-menu = document.getElementById('bbq_popup'); // hidden as default
-bbq = document.getElementById('bbq');
+//Javascript for individual menu items
 
-bbq.addEventListener('click',() =>{
-    menu.classList.add('show')
-    // alert('this works')
+partyA_display = document.getElementById('partyA_popup'); // hidden as default
+partyA_click = document.getElementById('partyA');
+
+partyA_click.addEventListener('click',() =>{
+    partyA_display.classList.add('show')
+
+})
+
+partyB_display = document.getElementById('partyB_popup'); // hidden as default
+partyB_click = document.getElementById('partyB');
+
+partyB_click.addEventListener('click',() =>{
+    partyB_display.classList.add('show')
+
+})
+
+lunchMenu_display = document.getElementById('lunchMenu_popup'); // hidden as default
+lunchMenu_click = document.getElementById('lunchMenu');
+
+lunchMenu_click.addEventListener('click',() =>{
+    lunchMenu_display.classList.add('show')
+
+})
+
+dressing_display = document.getElementById('dressing_popup'); // hidden as default
+dressing_click = document.getElementById('dressing');
+
+dressing_click.addEventListener('click',() =>{
+    dressing_display.classList.add('show')
+
+})
+
+officeCatering_display = document.getElementById('officeCatering_popup'); // hidden as default
+officeCatering_click = document.getElementById('office_catering');
+
+officeCatering_click.addEventListener('click',() =>{
+    officeCatering_display.classList.add('show')
+
+})
+
+everydayExtras_display = document.getElementById('everyday_extras_popup'); // hidden as default
+everydayExtras_click = document.getElementById('everyday_extras');
+
+everydayExtras_click.addEventListener('click',() =>{
+    everydayExtras_display.classList.add('show')
+
+})
+
+execMenuA_display = document.getElementById('exec_menuA_popup'); // hidden as default
+execMenuA_click = document.getElementById('exec_menuA');
+
+execMenuA_click.addEventListener('click',() =>{
+    execMenuA_display.classList.add('show')
+
+})
+
+execMenuB_display = document.getElementById('exec_menuB_popup'); // hidden as default
+execMenuB_click = document.getElementById('exec_menuB');
+
+execMenuB_click.addEventListener('click',() =>{
+    execMenuB_display.classList.add('show')
+
+})
+
+execMenuC_display = document.getElementById('exec_menuC_popup'); // hidden as default
+execMenuC_click = document.getElementById('exec_menuC');
+
+execMenuC_click.addEventListener('click',() =>{
+    execMenuC_display.classList.add('show')
+
+})
+
+breakfast_display = document.getElementById('breakfast_popup'); // hidden as default
+breakfast_click = document.getElementById('breakfast');
+
+breakfast_click.addEventListener('click',() =>{
+    breakfast_display.classList.add('show')
+
+})
+
+bbq_display = document.getElementById('bbq_popup'); // hidden as default
+bbq_click = document.getElementById('bbq');
+
+bbq_click.addEventListener('click',() =>{
+    bbq_display.classList.add('show')
+    
 })
 
 // click on a section
