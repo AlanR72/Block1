e40eaca1d3544af54018eedf10e74f6cd8f3c4cb
Alan <robbalan72@tiscali.co.uk>
2025-01-13 21:01:14 +0000

    Updated style.css

--- dignans_site/js/main-MS-13-22-HPE.js
+++
@@ -1,9 +0,0 @@
-function Nav(){
-    let mobileClick = document.getElementById('mobile-click');
-    let mobileNav = document.querySelector('#mobile-nav');
-
-    mobileClick.addEventListener('click',() =>{
-        mobileNav.classList.toggle('active');
-    });
-}
-Nav()
